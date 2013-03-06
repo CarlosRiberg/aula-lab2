2ff67c83e00b3c6c7b7a726111f7dee36a0862eb
Carlos <Carlos@192.168.1.4>
2013-03-06 17:49:46 -0300

    V.1.1

--- entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
+++ entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
@@ -1,32 +1,40 @@
 package br.com.senacrs.alp.aulas;
 
-
 public class Somatorio {
-	
+
 	private static Somatorio instancia = new Somatorio();
-	
-	
+
 	private Somatorio() {
 
 		super();
 	}
-	
+
 	public double somaTotal(double[] valores) {
-		
+
 		double resultado = 0.0;
-		
+
+		// Implementar
+
 		if (valores == null) {
-			
+
 			resultado = Double.NaN;
+
+		}
+
+		valores = new double[10];
+		for (int i = 0; i < valores.length; i++) {
+			double val = Math.random();
+			valores[i] = val;
+			resultado += val;
 		}
 		
-		//Implementar
-		
+		resultado = valores[0];
+
 		return resultado;
 	}
-	
+
 	public static Somatorio getInstancia() {
-		
+
 		return Somatorio.instancia;
 	}
 }
