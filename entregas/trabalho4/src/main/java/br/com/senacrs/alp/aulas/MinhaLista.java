897555be58da5b75a48fca074ea3043d930c87b9
631210378 <631210378@Sala603-21.fspoaeduc.com.br>
2013-03-28 19:18:26 -0300

    Trabalho 4 final

--- entregas/trabalho4/src/main/java/br/com/senacrs/alp/aulas/MinhaLista.java
+++ entregas/trabalho4/src/main/java/br/com/senacrs/alp/aulas/MinhaLista.java
@@ -1,98 +1,237 @@
 package br.com.senacrs.alp.aulas;
 
-public class MinhaLista<T> implements Lista<T> {
 
-	// com base na logica do meu colega eu tentei fazer o trabalho 4
-	// desculpe por não ter me empenhado mais!
 
-	Nodo nodo = new Nodo();
-	Nodo nodoNovo = new Nodo();
-	Nodo nodoAnterior = new Nodo();
-	Nodo<T> inicio = null;
+
+public class MinhaLista<T> implements Lista<T>{
+
+	
+
+	 private Nodo<T> inicio = new Nodo<T>();
+ 
+	
 
 	@Override
-	public void adicionarFinal(T valor) {
-		// TODO Auto-generated method stub
-		adicionarPosicao((Integer) obterPosicao((Integer) null), valor);
 
+	 public void adicionarFinal(T valor) {
+ 
+		adicionarPosicao(obterTamanho(), valor);
+
+		
+
+	 }
+ 
+
+
+
+	@Override
+
+	 public void adicionarInicio(T valor) {
+
+		 adicionarPosicao(0, valor);
+ 
 	}
 
-	@Override
-	public void adicionarInicio(T valor) {
-		// TODO Auto-generated method stub
-		adicionarPosicao(0, valor);
 
+
+
+	 @Override
+
+	 public void adicionarPosicao(int posicao, T valor) {
+ 
+		Nodo<T> nodo = null;
+
+		 Nodo<T> nodoNovo = new Nodo<T>();
+
+ 		if(valor == null){
+
+			 throw new IllegalArgumentException();
+
+		 }
+ 
+		if(posicao < 0){
+
+			 throw new IllegalArgumentException();
+
+		 }
+ 
+		if(posicao > obterTamanho()){
+
+			 throw new IllegalArgumentException();
+
+ 		}
+
+		 nodo = obterNodoPosicao(posicao -1);
+
+		 nodoNovo.proximo = nodo.proximo;
+ 
+		nodo.proximo = nodoNovo;
+
+		 nodoNovo.conteudo = valor;
+
+	 }
+ 
+
+
+
+	@Override
+
+	 public T obterPrimeiro() {
+
+		 Nodo<T> nodo = null;
+ 
+		if(obterNodoPosicao(0) == null){
+
+			 throw new IllegalArgumentException();
+
+ 		}
+
+		 nodo = obterNodoPosicao(0);
+
+		 return nodo.conteudo;
+ 
 	}
 
-	@Override
-	public void adicionarPosicao(int posicao, T valor) {
-		// TODO Auto-generated method stub
-		inicio = (Nodo<T>) obterPosicao(posicao);
-		inicio.conteudo = valor;
-		nodoNovo = inicio;
 
-	}
 
-	@Override
-	public T obterPrimeiro() {
-		// TODO Auto-generated method stub
-		inicio = (Nodo<T>) obterPosicao(0);
-		return inicio.conteudo;
 
-	}
+	 @Override
 
-	@Override
-	public T obterUltimo() {
-		// TODO Auto-generated method stub
+	 public T obterUltimo() {
+ 
 		int tamanho = obterTamanho();
-		T resultado = obterPosicao(tamanho - 1);
-		return resultado;
 
+		 T resultado = obterPosicao(tamanho -1);
+
+		 return resultado;
+ 
 	}
 
-	@Override
+
+
+
+	
+
+	 @Override
+ 
 	public T obterPosicao(int posicao) {
-		// TODO Auto-generated method stub
-		int indice = -1;
-		while (posicao != indice) {
-			nodo = nodo.proximo;
-		}
 
-		return (T) nodo.conteudo;
+		 Nodo<T> nodo = null;
 
-	}
+		 if(posicao < 0 ){
+ 
+			throw new IllegalArgumentException();
 
-	@Override
+		 }
+
+		 if(posicao >= obterTamanho()){
+ 
+			throw new IllegalArgumentException();
+
+		 }
+
+		 nodo = obterNodoPosicao(posicao);
+ 
+		return nodo.conteudo;
+
+	 }
+
+
+
+
+	 @Override
+ 
 	public int obterTamanho() {
-		// TODO Auto-generated method stub
-		int resultado = 0;
-		while (obterPosicao(resultado) != null) {
+
+		 int resultado = 0;
+
+		 Nodo<T> nodo = null;
+ 
+		nodo = inicio;
+
+		 while(nodo.proximo != null){
+
+			 nodo = nodo.proximo;
+ 
 			resultado++;
-		}
 
+		 }
+
+		
+ 
 		return resultado;
-	}
 
-	@Override
+	 }
+
+
+
+
+	 @Override
+ 
 	public T removerPosicao(int posicao) {
-		// TODO Auto-generated method stub
 
-		nodoAnterior.proximo = (Nodo) obterPosicao(posicao - 1);
-		nodoNovo = (Nodo) obterPosicao(posicao + 1);
-		nodoAnterior = nodoNovo;
-		nodo = nodoAnterior;
-		nodoNovo = (Nodo) obterPosicao(posicao);
-		nodoNovo.proximo = null;
+		 Nodo<T> nodo = null;
 
-		return (T) nodo.conteudo;
-	}
+		 Nodo<T> nodoAnterior = new Nodo<T>();
+ 
+		if(posicao < 0 ){
 
-	@Override
+			 throw new IllegalArgumentException();
+
+		 }
+ 
+		if(posicao >= obterTamanho()){
+
+			 throw new IllegalArgumentException();
+
+ 		}
+
+		 nodo = obterNodoPosicao(posicao);
+
+		 nodoAnterior = obterNodoPosicao(posicao -1);
+ 
+		nodoAnterior.proximo = nodo.proximo;
+
+		 nodo.proximo = null;
+
+		 T resultado = nodo.conteudo;
+ 
+		return resultado;
+
+	 }
+
+
+
+
+	 @Override
+ 
 	public void esvaziar() {
-		// TODO Auto-generated method stub
 
-		inicio.proximo = null;
+		 this.inicio.proximo = null;
 
+	 }
+ 
+	
+
+	private Nodo<T> obterNodoPosicao(int posicao){
+
+		 Nodo<T> nodo = inicio;
+ 
+		int indice = -1;
+
+		 while(posicao != indice){
+
+			 nodo = nodo.proximo;
+ 
+			indice++;
+
+		 }
+
+		 return nodo;
+ 
 	}
+
+
+
 
 }
