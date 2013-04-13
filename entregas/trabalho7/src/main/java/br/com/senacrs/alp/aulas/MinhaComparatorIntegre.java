adbe025c44899f6d9df38193f1e5d4fe29bc44c8
Carlos <Carlos@Dragon-PC>
2013-04-13 17:45:01 -0300

    Trabalho7 V1.5

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MinhaComparatorIntegre.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MinhaComparatorIntegre.java
@@ -6,23 +6,11 @@ public class MinhaComparatorIntegre<T> implements Comparator<Integer> {
 
 	@Override
 	public int compare(Integer arg1, Integer arg2) {
-		
-		if (arg1 > arg2) {
-				   return 1;
-		}
-				else if(arg1 < arg2){
-				   return -1;
-				}
 
-				else{
-				    return 0;
-				}
-			
+		if (arg1 < arg2) {
+			return 1;
+		} else {
+			return -1;
 		}
-			
-		
-		
-		//return 0;
-	//}
-
+	}
 }
--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MinhaComparatorString.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MinhaComparatorString.java
@@ -6,8 +6,8 @@ public class MinhaComparatorString<T> implements Comparator<String> {
 
 	@Override
 	public int compare(String o1, String o2) {
-		
+
 		return o1.compareTo(o2) * -1;
-	
+
 	}
 }
