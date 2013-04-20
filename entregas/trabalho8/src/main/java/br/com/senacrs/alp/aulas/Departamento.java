9e81e0487f9cd19661e037113fa3ab683960e6da
Carlos <Carlos@Dragon-PC>
2013-04-20 15:00:15 -0300

    Trabalho8 Feito

--- entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/Departamento.java
+++ entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/Departamento.java
@@ -16,3 +16,25 @@ public interface Departamento {
 	
 	void adicionarFuncionario(Funcionario funcionario);
 }
+
+
+
+/*package br.com.senacrs.alp.aulas;
+
+import java.util.List;
+
+public interface Departamento {
+
+	Empresa getEmpresa();
+	
+	String getNome();
+	
+	List<Funcionario> listarFuncionariosPorOrdemAlfabetica();
+	
+	List<Funcionario> listarFuncionariosPorDecrescenteSalario();
+	
+	int quantidadeFuncionarios();
+	
+	void adicionarFuncionario(Funcionario funcionario);
+}
+*/
