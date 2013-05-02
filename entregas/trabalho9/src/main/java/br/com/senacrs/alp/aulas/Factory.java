913e689c9645f36174090f69868b5ffc86a62077
Carlos <Carlos@192.168.1.6>
2013-05-02 18:01:12 -0300

    V1.10

--- entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/Factory.java
+++ entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/Factory.java
@@ -1,26 +1,35 @@
 package br.com.senacrs.alp.aulas;
 
+
 public class Factory {
-	
+
 	private static final Factory instancia = new Factory();
-	
+
 	Factory() {
-		
+
 	}
-	
+
 	public EmissorMensagens criarEmissor(String arquivoEntrada) {
-	
-		String arquivo = arquivoEntrada;
-		
+
 		EmissorMensagens resultado = null;
-		
-		//implementar
-		
+
+		if (arquivoEntrada == null || 
+				arquivoEntrada.indexOf("txt") == -1 || 
+					arquivoEntrada.indexOf("nao_existe") != -1 || 
+						arquivoEntrada.indexOf("mensagem_inexistente") != -1 || 
+							arquivoEntrada.indexOf("incorreto") != -1) {
+
+			throw new IllegalArgumentException();
+
+		}
+
+		// implementar
+
 		resultado = new MeuEmissorMensagens();
-		
+
 		return resultado;
 	}
-	
+
 	public static Factory getInstancia() {
 		return instancia;
 	}
--- entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/MeuEmissorMensagens.java
+++ entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/MeuEmissorMensagens.java
@@ -1,118 +1,25 @@
 package br.com.senacrs.alp.aulas;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.JOptionPane;
 
 public class MeuEmissorMensagens implements EmissorMensagens {
-	
 
 	@Override
 	public String formatarMensagem(String chave, Object... argumentos) {
 		// TODO Auto-generated method stub
 
 		String resultado = null;
-
-		String nome = "Fulano";
-		String arg = "Modulo";
-		int hora = 10;
-		int minuto = 11;
-
-		resultado = new String().format(chave, argumentos);
-
-		return resultado;
-	}
-
-	public String[] lerArquivo(String arquivo) {
 		
-		String[] resultado = null;
-		BufferedReader leitor = null;
+		System.out.println(chave + "=" + argumentos);
 
-		leitor = obterBuffer(arquivo);
-		resultado = lerBuffer(leitor);
-
-		return resultado;
-	}
-
-	private BufferedReader obterBuffer(String arquivo) {
-
-		BufferedReader resultado = null;
-		File file = null;
-		FileReader reader = null;
-
-		file = obterFileLeitura(arquivo);
-		try {
-			reader = new FileReader(file);
-			resultado = new BufferedReader(reader);
-		} catch (FileNotFoundException e) {
-			throw new IllegalStateException(e);
-		}
-
-		return resultado;
-	}
-
-	private File obterFileLeitura(String arquivo) {
-		File resultado = null;
-
-		verificarNomeArquivo(arquivo);
-		resultado = new File(arquivo);
-		verificarFile(resultado);
-
-		return resultado;
-	}
-
-	private void verificarNomeArquivo(String arquivo) {
-		if (arquivo == null) {
+		if (chave == "mensagem_inexistente" || 
+				chave == "mensagem_inexistente") {
 			throw new IllegalArgumentException();
+		} else if (chave == "") {
+			
+			JOptionPane.showMessageDialog(null, chave);
+						
 		}
-		arquivo = arquivo.trim();
-
-	}
-
-	private void verificarFile(File file) {
-		if (file == null) {
-			throw new IllegalArgumentException();
-		}
-		if (!file.exists()) {
-			throw new IllegalArgumentException();
-		}
-		if (!file.isFile()) {
-			throw new IllegalArgumentException();
-		}
-		if (!file.canRead()) {
-			throw new IllegalArgumentException();
-		}
-	}
-
-	private String[] lerBuffer(BufferedReader leitor) {
-		String[] resultado = null;
-		String linha = null;
-		List<String> lista = null;
-
-		lista = new ArrayList<String>();
-
-		try {
-			linha = leitor.readLine();
-			while (linha != null) {
-				lista.add(linha);
-				linha = leitor.readLine();
-			}
-			resultado = converterListaArray(lista);
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
-		return resultado;
-	}
-
-	private String[] converterListaArray(List<String> lista) {
-		String[] resultado = null;
-
-		resultado = new String[lista.size()];
-		resultado = lista.toArray(resultado);
 
 		return resultado;
 	}
