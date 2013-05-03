5a04fbd250c652353f95a06409db865175895943
Carlos <Carlos@192.168.1.2>
2013-05-03 15:54:18 -0300

    Trabalho 10 V1.1

---
+++ entregas/trabalho10/src/main/java/br/com/senacrs/alp/aulas/Factory.java
@@ -0,0 +1,24 @@
+package br.com.senacrs.alp.aulas;
+
+public class Factory {
+
+	private static final Factory instancia = new Factory();
+
+	private Factory() {
+	}
+
+	public ListaConteudoDiretorio criar() {
+
+		ListaConteudoDiretorio resultado = null;
+
+		resultado = new MinhaListaConteudoDiretorio();
+
+		// implementar
+
+		return resultado;
+	}
+
+	public static Factory getInstancia() {
+		return instancia;
+	}
+}
